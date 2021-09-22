04947871b65d52dfe256e5438f6d3a836fe967f6
Alexander Welton <71471789+SchoolPotato@users.noreply.github.com>
2021-09-22 13:08:45 -0700

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Switch } from "react-router-dom";
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
